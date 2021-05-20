7de841c48fec6d1cecbfd7d3506ab03da75fab97
Vaish1591 <70366759+Vaish1591@users.noreply.github.com>
2021-05-20 19:00:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import flask
-from flask import request
+import pickle
+from flask import request, Flask, render_template, jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 from flask_cors import CORS
 CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('hp.html')
 
 @app.route('/predict',methods=['GET'])
 def predict():
